17a45b8780437fd6747b73e02a955f9907f07a65
Minu Jeong <mjung@ea.com>
2016-02-21 10:04:44 +0900

    add npc talk

---
+++ Assets/Internals/Scripts/NPCTalk.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class NPCTalk : MonoBehaviour
+{
+
+	// Use this for initialization
+	void OnEnable ()
+	{
+		StartCoroutine ("WaitHide");
+
+		transform.localScale = Vector3.zero;
+
+		transform.DOScale (Vector3.one, 0.2F);
+	}
+
+	IEnumerator WaitHide ()
+	{
+		yield return new WaitForSeconds (1.0F);
+
+		transform.DOScale (Vector3.zero, 0.2F);
+
+		yield return new WaitForSeconds (0.5F);
+
+		gameObject.SetActive (false);
+	}
+}
+
